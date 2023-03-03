4ee0346b1a8f31ab537441792d4c2840e979085a
Luis A Gomez-Tinoco <luchoagomezt@users.noreply.github.com>
2023-03-03 16:31:13 -0500

    Update weather.py

--- lib/weather.py
+++ lib/weather.py
@@ -1,6 +1,6 @@
 import requests
-import json
-import math
+API_ID = "55f5ab2e144704e42c8bf3522249b921"
+API_END_POINT = "https://api.openweathermap.org/data/2.5/weather?q=<city>,ca&appid=<api id>"
 
 
 class Weather:
@@ -8,13 +8,21 @@ class Weather:
         self.city = city
 
     def get_weather_forecast(self) -> str:
-        self.city
-        return None
+        response = requests.get(API_END_POINT.replace("<city>", self.city).replace("<api id>", API_ID))
+        assert response.status_code == 200
+        return response.json()["weather"]
+
+    def get_visibility(self) -> int:
+        response = requests.get(API_END_POINT.replace("<city>", self.city).replace("<api id>", API_ID))
+        assert response.status_code == 200
+        return response.json()["visibility"]
 
     def get_max_temperature(self) -> int:
-        self.city
-        return 200
+        response = requests.get(API_END_POINT.replace("<city>", self.city).replace("<api id>", API_ID))
+        assert response.status_code == 200
+        return response.json()["main"]["temp_max"] - 273.15
 
     def get_min_temperature(self) -> int:
-        self.city
-        return 200
+        response = requests.get(API_END_POINT.replace("<city>", self.city).replace("<api id>", API_ID))
+        assert response.status_code == 200
+        return response.json()["main"]["temp_min"] - 273.15
